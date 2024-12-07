7650fc610da2cf3e936259f610e5b6001f3f469a
cristian.diaz02@epn.edu.ec <cristian.diaz02@epn.edu.ec>
2024-12-07 15:28:28 -0500

    actualizacion master 07122024

--- src/index.js
+++ src/index.js
@@ -1,49 +1,28 @@
 const express = require('express');
-const admin = require('firebase-admin');
-const fs = require('fs'); // Para leer el archivo desde el sistema de archivos
-
 const app = express();
+const cors = require('cors'); 
+const port = 3000;
 
-// Middleware para parsear JSON
+// Importa los servicios
+const verifyToken = require('./servicios/verificaciontoken');
+const registerUser = require('./servicios/registrarusuario');
+const iniciarSesion = require('./servicios/iniciarsesion');
+
+// Middleware para analizar el cuerpo de las solicitudes en formato JSON
 app.use(express.json());
+app.use(cors());
 
-// Ruta al archivo secreto en Render
-const serviceAccountPath = '/etc/secrets/serviceAccountKey.json';
+// Usa los servicio
+app.use('/verifyToken', verifyToken);
+app.use('/registerUser', registerUser);
+app.use('/signin', iniciarSesion);
 
-// Inicializar Firebase Admin SDK
-admin.initializeApp({
-    credential: admin.credential.cert(require(serviceAccountPath))
-});
-
-const db = admin.firestore();
-
-const PORT = process.env.PORT || 3000; // Render define PORT
-
-// Ruta principal
+// Ruta básica para verificar que el servidor está corriendo
 app.get('/', (req, res) => {
-    res.send('¡Hola, mundo! Tu servidor está funcionando.');
+  res.send('Bienvenido al servidor backend.');
 });
 
-// Endpoint para verificar si un usuario está registrado
-app.post('/api/isUserRegistered', async (req, res) => {
-    const { email } = req.body;
-    if (!email) return res.status(400).send({ error: 'Email es requerido.' });
-
-    try {
-        // Buscar el documento del usuario por email
-        const userDoc = await db.collection('users').doc(email).get();
-        if (userDoc.exists) {
-            return res.status(200).send({ registered: true });
-        } else {
-            return res.status(200).send({ registered: false });
-        }
-    } catch (error) {
-        console.error('Error verificando registro:', error);
-        return res.status(500).send({ error: 'Error interno del servidor.' });
-    }
-});
-
-// Iniciar el servidor
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Inicia el servidor
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
