c1831d4d70da905e05368c88722f53294c301c74
cristian.diaz02@epn.edu.ec <cristian.diaz02@epn.edu.ec>
2024-12-22 23:56:06 -0500

    correccion error

--- src/servicios/comprobarusuario.js
+++ src/servicios/comprobarusuario.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const comprobarUsuarioYRecuperarDatos = async (uid) => {
+const comprobarusuario = async (uid) => {
   const db = admin.firestore();
   const userRef = db.collection('usuario').doc(uid);
   const doc = await userRef.get();
