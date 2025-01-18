40fc726cabf19de3fd8d8689f7a4eba3a814c64e
cristian.diaz02@epn.edu.ec <cristian.diaz02@epn.edu.ec>
2025-01-18 01:03:49 -0500

    busqueda de catalogos 2.0

--- src/servicios/buscarproductocatalogo.js
+++ src/servicios/buscarproductocatalogo.js
@@ -1,4 +1,3 @@
-//buscarproducto.js
 const express = require('express');
 const admin = require('../firebase'); // Importa la configuración de Firebase desde firebase.js
 
@@ -9,19 +8,19 @@ router.get('/', async (req, res) => {
   const nombreCatalogo = req.query.catalogo; // No convertir a minúsculas
 
   if (!nombreCatalogo) {
-    return res.status(400).json({ error: 'Por favor proporciona un nombre de producto.' });
+    return res.status(400).json({ error: 'Por favor proporciona un nombre de catálogo.' });
   }
 
   try {
-    console.log('Buscando productos:', nombreCatalogo);
+    console.log('Buscando productos del catálogo:', nombreCatalogo);
     const productosSnapshot = await db
       .collection('producto')
       .where('catalogo', '==', nombreCatalogo)
       .get();
 
     if (productosSnapshot.empty) {
-      console.log('No se encontró ningún producto con ese nombre.');
-      return res.status(404).json({ error: 'Producto no encontrado.' });
+      console.log('No se encontraron productos para el catálogo:', nombreCatalogo);
+      return res.status(404).json({ error: 'No se encontraron productos para este catálogo.' });
     }
 
     const productos = productosSnapshot.docs.map(doc => ({
@@ -29,10 +28,10 @@ router.get('/', async (req, res) => {
       ...doc.data()
     }));
 
-    res.json(productos[0]); // Asumiendo que solo hay un producto por nombre
+    res.json(productos); // Devolver todos los productos encontrados
   } catch (error) {
-    console.error('Error buscando los productos:', error);
-    res.status(500).json({ error: 'Error buscando los productos.' });
+    console.error('Error buscando los productos por catálogo:', error);
+    res.status(500).json({ error: 'Error buscando los productos por catálogo.' });
   }
 });
 
