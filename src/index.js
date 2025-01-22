322524b929eda183e0ced575c51c774c552812e5
cristian.diaz02@epn.edu.ec <cristian.diaz02@epn.edu.ec>
2025-01-22 18:22:02 -0500

    reservar productos

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ const validarDatos = require('./servicios/validarDatos');
 const createproduct = require('./servicios/crearproducto');
 const modifyproduct = require('./servicios/modificarproducto');
 const deleteproduct = require('./servicios/eliminarproducto');
+const reservarStockTemporal = require('./servicios/reservarstock');
 
 // Middleware de rutas de pagos
 app.use('/payment', paymentRoutes);
@@ -68,6 +69,7 @@ app.use('/validacionDatos', validarDatos);
 app.use('/createproduct', createproduct);
 app.use('/modifyproduct', modifyproduct);
 app.use('/deleteproduct', deleteproduct);
+app.use('/reservas', reservarStockTemporal);
 
 // Ruta básica para verificar que el servidor está corriendo
 app.get('/', (req, res) => {
